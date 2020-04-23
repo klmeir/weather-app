15dde287d63e43081d9010407ab13bb518506ee6
klmeir <caalmeir@gmail.com>
2020-04-23 12:41:58 -0500

    Add more validations

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,11 +2,19 @@ import React from "react";
 import Location from "./Location";
 import WeatherData from "./WeatherData";
 import "./styles.css";
+import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY } from "./../../constants/weathers";
+
+const data = {
+  temperature: 32,
+  weatherState: CLOUDY,
+  humidity: 80,
+  wind: "10 m/s",
+};
 
 const WeatherLocation = () => (
   <div className="weatherLocationCont">
     <Location city={"Cartagena"}></Location>
-    <WeatherData></WeatherData>
+    <WeatherData data={data}></WeatherData>
   </div>
 );
 
